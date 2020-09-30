6d4ba24fd0e5ebc3f2e9c8492cc68dc89709675f
zlwww <32141949+zlwww@users.noreply.github.com>
2020-10-01 00:51:27 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -28,43 +28,6 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 rm -rf autocore
 svn co https://github.com/project-openwrt/openwrt/branches/18.06-kernel5.4/package/lean/autocore
 popd
-
-# 校园网锐捷 Add mentohust & luci-app-mentohust.
-# git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
-# git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
-
-# Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
-
-# 京东签到 Add luci-app-jd-dailybonus
-git clone --depth=1 https://github.com/jerrykuku/node-request
-git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
-
-# Add luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
-
-# 微信推送 Add ServerChan
-git clone --depth=1 https://github.com/tty228/luci-app-serverchan
-
-# Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
-
-# Add luci-app-onliner (need luci-app-nlbwmon)
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
-
-# 磁盘管理 Add luci-app-diskman
-# git clone --depth=1 https://github.com/lisaac/luci-app-diskman
-# mkdir parted
-# cp luci-app-diskman/Parted.Makefile parted/Makefile
-
-
-# Add smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
-
-
-
 # Mod zzz-default-settings
 pushd package/lean/default-settings/files
 sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" zzz-default-settings
